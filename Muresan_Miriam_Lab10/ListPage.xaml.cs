e1d73ae9e0f6667908ac8dc642178e9ed8c33c34
Raluca Miriam Muresan <miriammuresan27@yahoo.ro>
2021-12-16 15:20:20 +0200

    Am realizat laboratorul 11

--- Muresan_Miriam_Lab10/ListPage.xaml.cs
+++ Muresan_Miriam_Lab10/ListPage.xaml.cs
@@ -31,5 +31,23 @@ namespace Muresan_Miriam_Lab10
         {
             InitializeComponent();
         }
+
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+           this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
     }
 }
